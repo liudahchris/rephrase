a7227c986fc6103ebc596b87686d890e4e7b998a
Chris Liu <chris.sj.liu@gmail.com>
2016-11-16 16:30:28 -0800

    november 16th work

--- code/classification/subset_clf.py
+++ code/classification/subset_clf.py
@@ -1,11 +1,14 @@
 import pandas as pd
+from collections import Counter
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.grid_search import GridSearchCV
 
 def main():
     data_path = '../../classification_data/subset_data/{}'
-    df = pd.read_csv(data_path.format('subset_features.csv'),index_col=0)
+    # fname = 'subset_features.csv'
+    fname = 'features_nov_16.csv'
+    df = pd.read_csv(data_path.format(fname),index_col=0)
     label = pd.read_csv(data_path.format('labeled_df.csv'),index_col=0)
     df = df.join(label)
     df['key'] = df.key.apply(str)
@@ -13,12 +16,15 @@ def main():
     y = df.pop('label').values
     X = df.values
     X_train, X_test, y_train, y_test = train_test_split(X,y)
-    rf_params = {'n_estimators':[10,500,1000,1500,2000],'max_depth':[3,5,7,9,11,None]}
-    rf = RandomForestClassifier(n_estimators=10,max_depth=5,class_weight='balanced',n_jobs=-1)
+    rf = RandomForestClassifier(n_estimators=500,max_depth=10,n_jobs=-1)
     rf.fit(X_train,y_train)
+    print rf.predict_proba(X_test)
+    print Counter(rf.predict(X_test))
+    print Counter(y_test)
     print rf.score(X_test,y_test)
+    # rf_params = {'n_estimators':[10,500,1000,1500,2000],'max_depth':[3,5,7,9,11,None]}
     # print 'Running Grid Search...'
-    # gs = GridSearchCV(rf,rf_params)
+    # gs = GridSearchCV(RandomForestClassifier(),rf_params)
     # gs.fit(X_train,y_train)
     # best_model = gs.best_estimator_
     # print gs.best_params_
--- code/make_features.py
+++ code/make_features.py
@@ -5,10 +5,18 @@ from collections import Counter,defaultdict
 import time
 import datetime
 import pandas as pd
+import numpy as np
 
 from setup_path import setup_path
 setup_path()
 import hdf5_getters as GETTERS
+
+def condense_segments(arr):
+    div = arr.shape[0]/4
+    averaged = []
+    for i in xrange(4):
+        averaged.append(np.mean(arr[i*div:(i+1)*div],axis=0))
+    return list(np.array(averaged).flatten())
 
 def apply_to_subset_files(basedir,func=lambda x: x,ext='.h5'):
     cnt = 0
@@ -40,10 +48,17 @@ def main():
                 GETTERS.get_mode,
                 GETTERS.get_tempo,
                 GETTERS.get_time_signature]
+        segment_funcs = [GETTERS.get_segments_pitches,
+                         GETTERS.get_segments_timbre,
+                         GETTERS.get_segments_loudness_max,
+                         GETTERS.get_segments_loudness_max_time]
         if track_id in tracks:
             row_data = [track_id]
             for func in funcs:
                 row_data.append(func(h5))
+            for func in segment_funcs:
+                segment_mat = func(h5)
+                row_data+=condense_segments(segment_mat)
             list_of_rows.append(row_data)
         h5.close()
         return None
@@ -55,10 +70,20 @@ def main():
 
     return list_of_rows
 
+def make_segment_cols(colname,num_vals):
+    s = 'seg{}'+colname+'{}'
+    return [s.format(i,j) for i in range(1,5) for j in range(1,num_vals+1)]
+
+
 if __name__=='__main__':
     msd_subset_path='/Users/christopherliu/Desktop/projects/music_project/data/MillionSongSubset'
     msd_subset_data_path=os.path.join(msd_subset_path,'data')
     msd_subset_addf_path=os.path.join(msd_subset_path,'AdditionalFiles')
     msd_code_path='/Users/christopherliu/Desktop/projects/music_project/helper_functions/MSongsDB'
+    segment_data = [('pitch',12),('timbre',12),('loudness',1),('loudness_time',1)]
     cols = ['track_id','duration','key','loudness','mode','tempo','time_sig']
+    for feat,num in segment_data:
+        cols+=make_segment_cols(feat,num)
     data = main()
+    df = pd.DataFrame(data=data,columns=cols).set_index('track_id')
+    df.to_csv('features_nov_16.csv')
