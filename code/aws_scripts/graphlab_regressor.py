9943e38ec310b60c06026516347a8bda305c1330
Chris Liu <chris.sj.liu@gmail.com>
2016-11-21 14:24:03 -0800

    edits

--- code/aws_scripts/graphlab_regressor.py
+++ code/aws_scripts/graphlab_regressor.py
@@ -1,5 +1,6 @@
 import graphlab as gl
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 
 if __name__=='__main__':
     data_dir = '../data/classification/{}'
@@ -9,6 +10,15 @@ if __name__=='__main__':
     del features
 
     train, test = sf.random_split(0.9)
-    del sf
     train = train.to_dataframe()
-    y_train = train.pop('labels')
+    y_train = train.pop('X1')
+    rf = RandomForestRegressor(n_estimators=100,verbose=1)
+    rf.fit(train,y_train)
+
+    del train
+    del y_train
+
+
+    test = test.to_dataframe()
+    y_test = test.pop('X1')
+    rf.score(test,y_test)
