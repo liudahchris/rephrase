85a39fa635dc54e4d3b21d06a203d126c95e2239
Chris Liu <chris.sj.liu@gmail.com>
2016-11-21 15:40:06 -0800

    graphlab stuff

--- code/aws_scripts/graphlab_regressor.py
+++ code/aws_scripts/graphlab_regressor.py
@@ -2,13 +2,7 @@ import graphlab as gl
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-if __name__=='__main__':
-    data_dir = '../data/classification/{}'
-    sf = gl.SFrame(data_dir.format('regression_values_sframe.csv'))
-    features = gl.SFrame(data_dir.format('aws_complete_features.csv'))
-    sf = sf.join(features, on='track_id')
-    del features
-
+def sklearn_regressor(sf):
     train, test = sf.random_split(0.9)
     y_train = train['X1'].to_numpy()
     train.remove_columns(['track_id','X1'])
@@ -22,4 +16,31 @@ if __name__=='__main__':
     y_test = test['X1'].to_numpy()
     test.remove_columns(['track_id','X1'])
     test = test.to_dataframe().fillna(-1000)
-    rf.score(test,y_test)
+    print rf.score(test,y_test)
+
+if __name__=='__main__':
+    data_dir = '../data/classification/{}'
+    sf = gl.SFrame(data_dir.format('regression_values_sframe.csv'))
+    features = gl.SFrame(data_dir.format('aws_complete_features.csv'))
+    sf = sf.join(features, on='track_id')
+    train,test = sf.random_split(0.9)
+
+    train_regress_vals = train['X1'].to_numpy()
+    train.remove_column('X1')
+    test_regress_vals = test['X1'].to_numpy()
+    test.remove_column('X1')
+
+    del features
+    del sf
+
+    models = []
+    preds = []
+    for i in xrange(regression_values.shape[1]):
+        train['target'] = train_regress_vals[:,i]
+        model = sf.random_forest_regression.create(train,target='target')
+        model.save('../models/model_{}'.format(i))
+        # test['target'] = test_regress_vals[:,i]
+        preds.append(model.predict(test).to_numpy())
+    preds = np.array(preds).T
+    np.savetxt('predicted_vals.txt',preds)
+    np.savetxt('actual_vals.txt',test_regress_vals)
