344e3faa3a330f76210eeac284a8b4f18d6fe27b
Chris Liu <chris.sj.liu@gmail.com>
2016-11-22 10:57:08 -0800

    edits to gridsearch

--- gridsearch/gridsearch.py
+++ gridsearch/gridsearch.py
@@ -1,6 +1,5 @@
 import graphlab as gl
 
-
 def load_data():
     d_path = '../data/classification/{}'
     features_path = d_path.format('aws_complete_features.csv')
@@ -9,25 +8,37 @@ def load_data():
     sf = gl.SFrame(labels_path)
     return sf.join(features,on='track_id')
 
-
-def main():
+def run_grid_search(params,outname=None):
     sf = load_data()
     train, test = sf.random_split(0.9)
     train, valid = train.random_split(0.8)
-
-    params = {'target': 'labels',
-             'class_weights': 'auto',
-             'max_depth': [5,10,20,30,40,50],
-             'max_iterations': [100,250,500,750,1000]
-             }
-
 
     job = gl.grid_search.create((train,valid),
                                 gl.boosted_trees_classifier.create,
                                 params
                                 )
 
-    job.save('gbm_gridsearch')
+    if outname:
+        job.save(outname)
+
+def test():
+    params = {'target': 'labels',
+             'class_weights': 'auto',
+             'max_depth': [3],
+             'max_iterations': [10]
+             }
+    outname = 'test'
+    run_grid_search(params,outname)
+
+def main():
+    params = {'target': 'labels',
+             'class_weights': 'auto',
+             'max_depth': [5,10,20,30,40,50],
+             'max_iterations': [100,250,500,750,1000]
+             }
+    outname = 'gbm_gridsearch'
+    run_grid_search(params,outname)
 
 if __name__=='__main__':
-    main()
+    # main()
+    test()
